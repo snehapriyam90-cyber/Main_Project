6f16f5af6810783c8971e6e81b0207e4f11a815a
snehapriyam90-cyber <your_email@example.com>
2026-01-04 01:10:19 +0530

    updated

--- organic_store_project/settings.py
+++ organic_store_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-79$%t10fw^8t$8yz7a)z_b9q2mmqr@=!(nuti#02v$*#hmh@s9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
