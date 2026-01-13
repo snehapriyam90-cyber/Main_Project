fcf8f87e319fd6a73383c9debe751b895e0fe1fc
snehapriyam90-cyber <your_email@example.com>
2026-01-13 11:51:03 +0530

    updated

--- organic_store_project/settings.py
+++ organic_store_project/settings.py
@@ -158,6 +158,6 @@ STATIC_URL = '/static/'
 
 #STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # PythonAnywhere will serve from here
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
