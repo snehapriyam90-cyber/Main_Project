212c0173bf565d8dc2177bdae58b1c87c452477f
snehapriyam90-cyber <your_email@example.com>
2026-01-13 11:53:14 +0530

    updated

--- organic_store_project/settings.py
+++ organic_store_project/settings.py
@@ -143,7 +143,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Razorpay
